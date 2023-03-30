0c8790864361846262e7cd1b552b36ebeda9c6e0
Sourav <sourav.sarkar@hih7.com>
2023-03-30 17:13:56 +0530

    fifth commit Add movie

--- src/App.js
+++ src/App.js
@@ -1,61 +1,64 @@
-import React, { useEffect, useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import MoviesList from './components/MoviesList';
 import './App.css';
 import Loader from './components/Loader';
-
+import { Button } from 'bootstrap';
+import AddMovie from './components/AddMovie';
 
 function App() {
-  const dummyMovies = [
-    {
-      id: 1,
-      title: 'Some Dummy Movie',
-      openingText: 'This is the opening text of the movie',
-      releaseDate: '2021-05-18',
-    },
-    {
-      id: 2,
-      title: 'Some Dummy Movie 2',
-      openingText: 'This is the second opening text of the movie',
-      releaseDate: '2021-05-19',
-    },
-  ];
 const [movieList , setMovieList] = useState([])
 const [isLoading , setIsLoading] = useState(false);
 const [error , setError] =useState(null)
 
-  useEffect(async ()=>{
-    setIsLoading(true)
-    await fetch('https://swapi.dev/api/films/').then((responce)=>{
-      if(!responce.ok){
-        throw new Error('SomeThing is Wrong here...');
-       }
-       return responce.json()
-    }).then((data)=>{
+  const fetchMiveHandler= useCallback(async ()=>{
+   setIsLoading(true)
+   try{
+   const responce = await fetch('https://swapi.dev/api/films/')
 
-      setIsLoading(false)
-          const movie = data.results.map((item)=>{
-            return {
-              id:item.id,
-              title:item.title,
-              openingText: item.opening_crawl,
-              releaseDate: item.release_date,
-            }
-          })
-          setMovieList(movie)
-    }).catch((err)=>{
-      setError(err.message)
-    })
+   if(!responce.ok){
+     throw new Error('SomeThing is Wrong here...');
+    }
+    let data = await responce.json();
+    setIsLoading(false)
+        const movie = data.results.map((item)=>{
+          return {
+            id:item.id,
+            title:item.title,
+            openingText: item.opening_crawl,
+            releaseDate: item.release_date,
+          }
+        })
+        setMovieList(movie)
+      }
+      catch(err){
+         
+       setError(err.message)
+      }
+      
     
   },[])
+
+  useEffect(()=>{
+fetchMiveHandler()
+  },[])
+
+  const movieData =(data)=>{
+    setMovieList([...movieList,data])
+  }
 
   return (
     <React.Fragment>
       <section>
+        <AddMovie onAddMovie={movieData}/>
+      </section>
+      <section>
+        <button onClick={fetchMiveHandler}>Fetch Movies</button>
       </section>
       <section>
         {isLoading ? <Loader/> : <MoviesList movies={movieList}/>}
         {isLoading && error && <p>{error}</p>}
+        
       </section>
     </React.Fragment>
   );
